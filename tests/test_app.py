74bc82f08ff53242b606d8f29c0e80944d917117
JaneAirVlad <124638379+JaneAirVlad@users.noreply.github.com>
2025-04-06 19:48:53 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -31,7 +31,7 @@ def test_docker_containers():
 
 # Тест 3: Проверка доступности базы данных
 def test_database_connection():
-    time.sleep(5)
+    time.sleep(30)
     try:
         conn = psycopg2.connect(
             dbname='mydatabase',  # Замените на имя вашей базы данных
